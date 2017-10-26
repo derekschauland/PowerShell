77357ced9173b19de2de3f0f20008b56cf4c3bf1
Derek Schauland <derek@derekschauland.com>
2017-10-26 16:22:23 -0500

    correcting broken function

--- check-archiveqty.ps1
+++ check-archiveqty.ps1
@@ -7,7 +7,4 @@ write-host " "
     write-host "Getting folder items for $user"
     Get-MailboxFolderStatistics -identity $user -archive | where {$_.name -eq $folder} | ft name, itemsinfolder
 
-    
-}
-
-
+}  
