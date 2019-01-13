ac251782b9a4b1696f92dceb83ea459341ef4551
Derek Schauland <derek@derekschauland.com>
2019-01-12 19:26:53 -0600

    Getting into git from Powershell studio

---
+++ scripts/check-archiveqty.ps1
@@ -0,0 +1,10 @@
+
+function check-archiveqty
+{
+    param($user,$folder)
+
+write-host " "
+    write-host "Getting folder items for $user"
+    Get-MailboxFolderStatistics -identity $user -archive | where {$_.name -eq $folder} | ft name, itemsinfolder
+
+}  
