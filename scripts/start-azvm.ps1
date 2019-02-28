08f6df26a0c3d0cec809b34242aedada711cb95b
Derek Schauland <dschauland@jminsure.com>
2019-02-28 15:39:38 -0600

    Initial commit for start-vm

---
+++ scripts/start-azvm.ps1
@@ -0,0 +1,61 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2019 v5.6.157
+	 Created on:   	2/28/2019 12:55 PM
+	 Created by:   	dschauland
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+
+Import-Module PowerShellGet
+Import-Module AzureRM.profile
+Function connectto-azure
+{
+	[cmdletbinding()]
+	
+	Param (
+		[Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)][string]$YourName
+	)
+	
+	If (!(Get-Module -Name az))
+	{
+		#az mod not here... load it
+		Install-Module -Name az -AllowClobber
+	}
+	
+	
+		Connect-AzAccount 
+			
+		
+	
+}
+
+Function Start-AzureVM
+{
+	[cmdletbinding()]
+	Param (
+		[string]$ResourceGroupName,
+		[string]$VMName,
+		[System.Management.Automation.PSCredential]$VMCreds
+		
+	)
+	
+	#Get the vm provided and start it
+	If ([string]::IsNullOrEmpty($VMname))
+	{
+		#No vm provided - check for vm and start
+		(get-azvm -resourcegroupname $ResourceGroupName) | start-azvm
+	}
+	Else
+	{
+		#vmname provided - start it up
+		
+		(get-azvm -resourcegroupname $ResourceGroupName -name $VMName) | start-azvm
+	}
+}
+
